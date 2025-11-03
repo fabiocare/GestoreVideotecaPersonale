14489401881062bb6073240bf1d4e6207c21f060
fabio <fabiocarello7@gmail.com>
2025-11-03 12:09:15 +0100

    Implementazione classe Videoteca Singleton + test JUnit

---
+++ src/main/java/org/example/model/Videoteca.java
@@ -0,0 +1,76 @@
+package org.example.model;
+
+import java.util.*;
+
+public class Videoteca {
+    private static Videoteca instance;
+    private final List<Film> films;
+
+    private Videoteca() {
+        this.films = new ArrayList<>();
+    }
+
+    public static Videoteca getInstance(){
+        if(instance == null){
+            instance = new Videoteca();
+        }
+        return instance;
+    }
+
+    //Operazioni principali
+    public void aggiungiFilm(Film film){
+        films.add(film);
+    }
+
+    public void rimuoviFilm(String titolo){
+        films.removeIf(f -> f.getTitolo().equals(titolo));
+    }
+
+    public void modificaFilm(String titolo,StatoVisione nuovoStato, int nuovaValutazione){
+        for (Film film : films) {
+            if (film.getTitolo().equals(titolo)) {
+                Film nuovoFilm = new Film.Builder(film.getTitolo(), film.getRegista(), film.getAnnoUscita())
+                        .genere(film.getGenere())
+                        .statoVisione(nuovoStato)
+                        .valutazione(nuovaValutazione)
+                        .build();
+                films.set(films.indexOf(film), nuovoFilm);
+                return;
+            }
+        }
+    }
+
+    //cerca e filtra
+    public List<Film> cercaPerTitolo(String titolo){
+        return films.stream().filter(f -> f.getTitolo().toLowerCase().contains(titolo.toLowerCase())).toList();
+    }
+
+    public List<Film> cercaPerRegista(String regista){
+        return films.stream().filter(f -> f.getRegista().toLowerCase().contains(regista.toLowerCase())).toList();
+    }
+
+    public List<Film> filtraPerGenere(Genere g){
+        return films.stream().filter(f -> f.getGenere() == g).toList();
+    }
+
+    public List<Film> filtraPerStatoVisione(StatoVisione sv){
+        return films.stream().filter(f -> f.getStatoVisione() == sv).toList();
+    }
+
+    public List<Film> filtraPerAnnoUscita(int anno){
+        return films.stream().filter(f -> f.getAnnoUscita() == anno).toList();
+    }
+
+    public void sostituisciFilms(List<Film> nuoviFilms){
+        films.clear();
+        films.addAll(nuoviFilms);
+    }
+
+    public List<Film> getTuttiFilms() {
+        return Collections.unmodifiableList(films);
+    }
+
+    public void svuotaVideoteca(){
+        films.clear();
+    }
+}
---
+++ src/test/java/org/example/model/VideotecaTest.java
@@ -0,0 +1,64 @@
+package org.example.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VideotecaTest {
+    private Videoteca videoteca;
+
+    @BeforeEach
+    void setUp(){
+        videoteca = Videoteca.getInstance();
+        videoteca.svuotaVideoteca();
+
+        Film filmBase = new Film.Builder("Oldboy", "Park Chan-wook", 2003)
+                .genere(Genere.THRILLER)
+                .valutazione(5)
+                .statoVisione(StatoVisione.VISTO)
+                .build();
+
+        videoteca.aggiungiFilm(filmBase);
+    }
+
+    @Test
+    void testAggiuntaFilm(){
+        List<Film> films= videoteca.getTuttiFilms();
+        assertEquals(1, films.size());
+        assertEquals("Oldboy", films.getFirst().getTitolo());
+    }
+
+    @Test
+    void testRimozioneFilm(){
+        videoteca.rimuoviFilm("Oldboy");
+        assertTrue(videoteca.getTuttiFilms().isEmpty());
+    }
+
+    @Test
+    void testModificaFilm(){
+        videoteca.modificaFilm("Oldboy", StatoVisione.DA_VEDERE, 4);
+        Film aggiornato = videoteca.getTuttiFilms().getFirst();
+        assertEquals(4,aggiornato.getValutazione());
+        assertEquals(StatoVisione.DA_VEDERE, aggiornato.getStatoVisione());
+    }
+
+    @Test
+    void TestFiltraPerGenere(){
+        List<Film> trovati = videoteca.filtraPerGenere(Genere.THRILLER);
+        assertEquals(1, trovati.size());
+    }
+
+    @Test
+    void testCercaPerTitolo(){
+        List<Film> trovati = videoteca.cercaPerTitolo("Oldboy");
+        assertFalse(trovati.isEmpty());
+        assertEquals("Park Chan-wook", trovati.getFirst().getRegista());
+    }
+
+    @Test
+    void testFiltraPerAnnoUscita(){
+        List<Film> trovati = videoteca.filtraPerAnnoUscita(2003);
+        assertEquals(1, trovati.size());
+    }
+}
